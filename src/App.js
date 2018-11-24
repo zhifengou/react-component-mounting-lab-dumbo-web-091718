20bf729be771f5a2a09632368c1c9b3e4a5207ca
zhifeng ou <zhifengou@hotmail.com>
2018-11-24 20:43:06 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -16,12 +16,6 @@ componentDidMount() {
      this.handleAddTimer()
    }
 
-
-
-
-
-
-
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
   render() {
